5b65fdbb8dc40a269425a3a4a438d04031639ce9
Magnus stampe Nielsen <magnusstampenielsen@magnuss-mbp.home>
2018-08-30 23:09:18 +0200

    w14d done

--- w1d4 - modal/mysolution/script.js
+++ w1d4 - modal/mysolution/script.js
@@ -1,9 +1,21 @@
 "use strict";
 
+document.querySelector("DOMContentLoaded", hideModal);
+
 function showModal() {
   console.log("Show modal");
+  document.querySelector("#modal_container").style = "display:flex";
 }
 
 function hideModal() {
   console.log("Hide modal");
+  document.querySelector("#modal_container").style = "display:none";
 }
+
+document.querySelector(".but1").addEventListener("click", showModal);
+document.querySelector(".but2").addEventListener("click", showModal);
+document.querySelector(".but3").addEventListener("click", showModal);
+document.querySelector(".but5").addEventListener("click", showModal);
+document.querySelector(".but6").addEventListener("click", showModal);
+
+document.querySelector("#closebutton").addEventListener("click", hideModal);
